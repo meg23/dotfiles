38f952273f787f00fca848fe4531f86d14145260
Max Gelman <max@luma=institute.com>
2021-04-23 09:10:16 -0400

    add brew installed packages / config updates.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+xargs brew install < brew-packages.txt
 ln -sf $(pwd)/.vimrc ~/.vimrc
-ln -sf $(pwd)/.screenrc ~/.screenrc
 ln -sf $(pwd)/.vim ~/.vim
 ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
